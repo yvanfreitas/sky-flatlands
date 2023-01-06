23e56e75b8b1e0ce02b61802dc86e18758fbe30e
yvanfreitas <yvan.freitas@gmail.com>
2023-01-06 01:43:45 -0300

    moving canvas to minimapClass

--- public/domain/render/minimap.js
+++ public/domain/render/minimap.js
@@ -1,11 +1,13 @@
-import { Move } from '../game/elements/actions.js';
-
 export default class MiniMap {
   context = null;
   tileSize = 2;
-  context = null;
 
-  constructor(screen) {
+  constructor() {
+    const screen = document.createElement('canvas');
+    screen.setAttribute('id', 'screen');
+    screen.setAttribute('width', 500);
+    screen.setAttribute('height', 500);
+    document.querySelector('body').appendChild(screen);
     this.context = screen.getContext('2d');
     this.render();
   }
@@ -14,22 +16,8 @@ export default class MiniMap {
     this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
 
     this.renderElements();
-
-    // this.renderCollisionGrid();
-
     requestAnimationFrame(() => {
       this.render();
-    });
-  }
-
-  renderCollisionGrid() {
-    window.core?.map?.grid?.forEach((row, x) => {
-      row.forEach((cell, y) => {
-        if (cell === 1) {
-          this.context.fillStyle = 'blue';
-          this.context.fillRect(x, y, this.tileSize, this.tileSize);
-        }
-      });
     });
   }
 
