208a9b53b9ccdccb3f616316cdca4ffc5d058ab0
yvanfreitas <yvan.freitas@gmail.com>
2023-01-20 00:48:14 -0300

    adding ssl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
+const privateKey = fs.readFileSync(path.resolve('src/ssl/server.key'));
+const certificate = fs.readFileSync(path.resolve('src/ssl/server.cert'));
+
+const credentials = { key: privateKey, cert: certificate };
 const app = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 app.use(express.static('public'));
 
-server.listen(3000, () => {
-  console.log(`> Pipipi popopo :3000`);
-});
+httpServer.listen(3000);
+httpsServer.listen(4000);
